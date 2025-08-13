caa136df117b773e84aee80772728d50a452a09e
ld-tecnica <alexandre.delazeri@gmail.com>
2025-08-13 16:27:40 -0300

    Deploy maldito! 2

--- gulpfile.js
+++ gulpfile.js
@@ -60,4 +60,4 @@ exports.scripts = scripts;
 // exports.images = images;
 exports.watch = watch;
 
-exports.default = gulp.parallel(styles, scripts, copyImages, html);
+exports.default = gulp.parallel(styles, scripts, copyImages, watch, html);
